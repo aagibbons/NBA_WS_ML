ee4169cc135a679c529e3e08af41b871a050f1e0
aagibbons <a.gibbons@mail.utoronto.ca>
2019-03-15 00:32:37 -0400

    Update test file.

--- Initial_Test_Python_File.py
+++ Initial_Test_Python_File.py
@@ -11,3 +11,12 @@ for t in range(3):
 
 for t in range(9):
     print(t*t)
+    if t > 4:
+        print("we in here")
+    else:
+        print('we not in here')
+
+x = True
+
+if x:
+    print("works")
