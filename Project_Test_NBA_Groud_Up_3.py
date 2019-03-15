a3c78450870a807e0286c3b13af2e3f1fde6587c
aagibbons <a.gibbons@mail.utoronto.ca>
2019-03-15 00:32:05 -0400

    Additional minor clean up changes.

--- Project_Test_NBA_Groud_Up_3.py
+++ Project_Test_NBA_Groud_Up_3.py
@@ -40,7 +40,7 @@ def get_features():
 # randomly select years for test set
 def rand_years_select(features, feature_list, threshold=0.20):
     
-    # Size of the dataset
+    # Size of the data set
     data_size = len(features)
     
     # obtain data on years
@@ -110,18 +110,22 @@ def rand_forest(train_features, test_features, train_labels, new_feature_list, r
     
     sorted_importances = []
     
-    # run the random forest model a certain number of times for more accuracy
+    # run the random forest model a certain number of times (runs) for more accuracy
     for b in range(runs):
+
+        # to track progress of the program
         print(b)
+
         # Instantiate model with chosen parameters
-        rf = RandomForestRegressor(n_estimators=n_estimators, max_depth = max_depth, random_state = b)
+        rf = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, random_state=b)
         
         # Train the model on training data
         rf.fit(train_features, train_labels)
         
         # Use the forest's predict method on the test data
         predictions = rf.predict(test_features)
-        
+
+        # build matrix to keep track of predictions
         if b == 0:
             predictions_matrix = np.array(predictions)
         else:
@@ -141,18 +145,21 @@ def rand_forest(train_features, test_features, train_labels, new_feature_list, r
 
 # first version of the evaluation procedure. Returns performance of average objective outcome for the first nth picks
 def eval_procedure_1(test_labels, test_years, predictions_matrix, years_dict):
+
     # create a matrix with the actual objective outcomes and the predicted outcomes
     if len(np.shape(predictions_matrix)) == 1:
         objective_actual_predicted = np.column_stack((test_labels, predictions_matrix))
     else:
         objective_actual_predicted = np.column_stack((test_labels, np.mean(predictions_matrix, 1)))
+
     # obtain locations of start of each draft year to isolate each year
     for b in range(len(test_years)):
         if b == 0:
             div_points = [years_dict[test_years[b]]]
         else:
             div_points.append(div_points[b-1]+years_dict[test_years[b]])
-    
+
+    # EXPLANATION NEEDED
     for c in range(len(test_years)):
         if c == 0:
             indiv_year = objective_actual_predicted[:div_points[c], :]
@@ -195,7 +202,9 @@ def rand_forest_eval_1_main(iterations=1, runs=5, max_depth=None):
     features = get_features()[0]
     feature_list = get_features()[1]
     for t in range(iterations):
+        # to track progress of the program
         print('-', t)
+
         test_years = rand_years_select(features, feature_list)[0]
         train_features = split_data(features, feature_list, test_years)[0]
         test_features = split_data(features, feature_list, test_years)[1]
@@ -222,10 +231,8 @@ def rand_forest_eval_1_main(iterations=1, runs=5, max_depth=None):
     return [pop_avg_matrix_actual, pop_avg_matrix_predicted, avg_pop_diff]
 
 
-output = rand_forest_eval_1_main(30, 5, 2)
+output = rand_forest_eval_1_main(5, 5, 2)
 
 # print result of how long program takes to run
 print()
 print("My program took", time.time() - start_time, "to run")
-
-
